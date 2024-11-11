8eaf1f3fd5693d60b5da6971ea6c2bb0ffa72dac
SergioMollo <sergio.rodriguez.casado@alumnos.uva.es>
2024-11-11 22:48:36 +0100

    Interfaz Crear Partida

--- Scripts/Controller/PantallaPrincipal.gd
+++ Scripts/Controller/PantallaPrincipal.gd
@@ -2,19 +2,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/CrearPartida.grab_focus()
-
-#func _on_nivel_1_pressed():
-	#get_tree().change_scene_to_file("res://Scenes/LaberintoNivel1.tscn")
-#
-#func _on_nivel_2_pressed():
-	#get_tree().change_scene_to_file("res://Scenes/LaberintoNivel2.tscn")
-#
-#func _on_nivel_3_pressed():
-	#get_tree().change_scene_to_file("res://Scenes/LaberintoNivel3.tscn")
-#
-#func _on_aleatorio_pressed():
-	#pass # Replace with function body.
+	$CanvasLayer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/CrearPartida.grab_focus()
+	
 
 func _on_crear_partida_pressed():
 	get_tree().change_scene_to_file("res://Scenes/CrearPartida.tscn")
@@ -36,7 +25,6 @@ func _on_ajustes_pressed():
 	add_child(instance)
 
 	
-
 func _on_salir_pressed():
 	get_tree().quit()
 
