e955861697af68346d3cef0922e82b50edd6a26f
SergioMollo <sergio.rodriguez.casado@alumnos.uva.es>
2025-03-10 19:07:48 +0100

    Mejora laberinto aleatorio

--- Maze/Controller/Game/AlgorithmController.gd
+++ Maze/Controller/Game/AlgorithmController.gd
@@ -158,29 +158,29 @@ func dijkstraSearch(start_node: Vector2, end_node: Vector2, avoid_position: Vect
 	parent = asign[1]
 
 	pushHeap(heap, [0, start_node])
-	# heap.append([0, start_node])
-	# visited.append(start_node)
 		
 	while heap:
 		var node = popHeap(heap)
 		var accumulated_cost = node[0]
 		var current_node = node[1]
 
-		if current_node not in visited:
-			visited.append(current_node)
+		if current_node in visited:
+			continue
 			
-			if current_node == end_node:
-				await printSearchTiles()
-				return await createPath(start_node, end_node, parent)
-			
-			for neighbor in get_neighbors(current_node):
-				var cost = getCost(neighbor, avoid_position)
-				var new_cost = accumulated_cost + cost
+		visited.append(current_node)
+		
+		if current_node == end_node:
+			await printSearchTiles()
+			return await createPath(start_node, end_node, parent)
+		
+		for neighbor in get_neighbors(current_node):
+			var cost = getCost(neighbor, avoid_position)
+			var new_cost = accumulated_cost + cost
 
-				if new_cost < distances[neighbor]:
-					distances[neighbor] = new_cost
-					parent[neighbor] = current_node
-					pushHeap(heap, [new_cost, neighbor])
+			if new_cost < distances[neighbor]:
+				distances[neighbor] = new_cost
+				parent[neighbor] = current_node
+				pushHeap(heap, [new_cost, neighbor])
 				
 	return []		
 
